7de05cb0c5fe0810931043b076b47a51b96df72b
regstuff <6016831+regstuff@users.noreply.github.com>
2022-07-29 21:59:39 +0530

    +exclude list

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ token = os.environ['TOKEN']
 owner = os.environ['OWNER']
 count = 0
 accepted = 0
-blacklist = ['viagra', 'sex', 'squirt', 'porn', 'sperm', 'nipple', 'condom', 'masturbat']
+blacklist = ['viagra', 'sex', 'squirt', 'porn', 'sperm', 'nipple', 'condom', 'masturbat', 'horny', 'virgin']
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/vnd.github+json"
